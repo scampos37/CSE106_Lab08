f0cf64cadad70853ce7a92e3567627fa500a477b
scampos37 <115961998+scampos37@users.noreply.github.com>
2023-04-21 00:48:54 -0700

    File upload
    Upload of completed lab files

---
+++ 106Lab8/queries.sql
@@ -0,0 +1,2 @@
+DELETE FROM roles_users
+WHERE user_id = 10;
---
+++ 106Lab8/static/myScript.js
@@ -0,0 +1,8 @@
+
+function addUser(){
+        uname = document.getElementById("uname").value,
+        password = document.getElementById("password").value
+        name = document.getElementById("name").value
+        teachORstudent = document.getElementById("teachORstudent").value
+    
+}
